f6690ac066bbea66a57d53d3635bb6db7f43fadf
judygab <ivditigabeskiria@gmail.com>
2022-05-18 22:30:52 -0400

    added code snippets

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
+import ComponentExamples from "../../../docs/component-examples.md";
+import CodeBlock from '@theme/CodeBlock';
 
 const FeatureList = [
   {
@@ -35,9 +37,113 @@ const FeatureList = [
   },
 ];
 
+const codeSnippetsList = [
+  {
+    title: 'Functional Component with Types',
+    description: (
+      <>
+      In TypeScript, Functional components that have Props, require their Props to be typed. There\s
+      several ways of declaring their types and creating a new Type specifially for component props is one of them:
+      </>
+    ),
+    codeBlock:  (`
+      type CardProps = {
+        title: string,
+        description: string
+      }
+
+      export const Card = ({ title, description }: CardProps) => <div>
+        <h2>{ title }</h2>
+        <p>
+          { description }
+        </p>
+      </div>
+      `)
+  },
+  {
+    title: 'Functional Component with Types and Defaults',
+    description: (
+      <>
+        Some or all props can be optional by adding question mark in the prop type definition. We can also
+        pass them a default value by doing the following:
+      </>
+    ),
+    codeBlock: `
+    type CardProps = {
+      title: string,
+      paragraph?: string  // the paragraph is optional
+    }
+
+    export const Card: FunctionComponent<CardProps> = ({ title = "Hello", paragraph }) =>
+      <div>
+        <h2>{ title }</h2>
+        <p>
+          { paragraph }
+        </p>
+      </div>
+    `
+  },
+  {
+    title: 'Functional Component with Typed Properties',
+    description: (
+      <>
+        We don't have to create a new type for every Props, we can also type them in-place inside the components.
+        Here's an example:
+      </>
+  ),
+  codeBlock: (
+    `
+    export const Button: FunctionComponent<CardProps> = ({ title, onClick } : {
+      title: string,
+      onClick: () => void
+    }) =>
+      <button onClick={onClick}>{title}</button>
+    `
+  )
+  },
+  {
+    title: 'Common Hooks',
+    description: (
+      <>
+        Let's see how some of the common hooks in React can be typed:
+      </>
+    ),
+    codeBlock: (
+      `
+      const [isOpen, setIsOpen] = useState<boolean>(false);
+      const [users, setUsers] = useState<IUser[] >([]);
+      `
+    )
+  },
+  {
+    title: 'Custom Hooks',
+    description: (
+      <>In custom hooks, we have to note that we need to also define return types. Since we return
+      one or more elements in the array, if we don't properly define each element type in order,
+      we will get a Type Error, because TypeScript assumes all elements of the array to be of the same type,
+      unless explicitely declared.
+      </>
+  ),
+    codeBlock:
+        (
+          `
+          import { useState } from "react";
+
+          export const useToggle = (): [boolean, () => void] => {
+            const [toggleOn, setToggleOn] = useState<boolean>(false);
+            const updateToggle = () => {
+              setToggleOn((prevToggleOn) => !prevToggleOn);
+            };
+            return [toggleOn, updateToggle];
+          };
+          `
+        )
+  }
+]
+
 function Feature({Svg, title, description}) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx('col')}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
@@ -49,14 +155,32 @@ function Feature({Svg, title, description}) {
   );
 }
 
+function Paragraph({title, description, codeBlock}) {
+  return (
+    <div className="row text--center">
+      <div className="col">
+        <div className="text--center padding-horiz--md">
+          <h3>{title}</h3>
+          <p>{description}</p>
+          <div className="text--left">
+            <CodeBlock language="tsx">{codeBlock}</CodeBlock>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
       <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
+        <div className="text--center">
+          {
+            codeSnippetsList.map((props, idx) => (
+              <Paragraph key={idx} {...props} />
+            ))
+          }
         </div>
       </div>
     </section>
