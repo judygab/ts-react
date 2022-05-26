65847a4a7ed61939a8dca1bcb41b0ff88e50db10
judygab <ivditigabeskiria@gmail.com>
2022-05-25 21:08:59 -0400

    updated config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,17 +8,17 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'React + TS',
   tagline: 'Code Snippets & Examples',
-  url: '/',
-  baseUrl: '/',
+  url: 'https://judygab.github.io',
+  baseUrl: '/ts-react/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'judygab', // Usually your GitHub org/user name.
+  projectName: 'ts-react', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
